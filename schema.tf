646b54cc9328711e37ffa2f072c91a496b7b67c9
KIRAN <ks@gmail.com>
2025-09-13 19:47:32 +0530

    User Secrets updated

---
+++ schema.tf
@@ -0,0 +1,16 @@
+resource "null_resource" "load-schema" {
+  depends_on = [aws_rds_cluster.default]
+
+  provisioner "local-exec" {
+    command = <<EOF
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+cd /tmp
+unzip -o mysql.zip
+cd mysql-main
+curl -L -O https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
+mongosh dev-db1-roboshop-docdb.cluster-c0xiwoigwrqk.us-east-1.docdb.amazonaws.com:27017 --tls --tlsCAFile global-bundle.pem --retryWrites=false --username ${local.DOCDB_USER} --password ${local.DOCDB_PASS} < catalogue.js
+mongosh dev-db1-roboshop-docdb.cluster-c0xiwoigwrqk.us-east-1.docdb.amazonaws.com:27017 --tls --tlsCAFile global-bundle.pem --retryWrites=false --username ${local.DOCDB_USER} --password ${local.DOCDB_PASS} < users.js
+EOF
+  }
+
+}
--- vars.tf
+++ vars.tf
@@ -5,7 +5,11 @@ variable "engine" {}
 variable "engine_version" {}
 variable "instance_class" {}
 variable "skip_final_snapshot" {}
+variable "nodes" {}
 variable "name" {}
+variable "vpc_id" {}
+variable "vpc_cidr" {}
+variable "BASTION_NODE" {}
 
 
 
